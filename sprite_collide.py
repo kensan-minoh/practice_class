70053192ce60926daa65bd4c571faca38dc88dbc
kensan-minoh <tanakakenzo2008@gmail.com>
2025-07-16 17:16:53 +0900

    some changes

--- sprite_collide.py
+++ sprite_collide.py
@@ -27,13 +27,53 @@ class Monster(pygame.sprite.Sprite):
     def update(self):
         self.rect.y += self.velocity
 
+class Knight(pygame.sprite.Sprite):
+    def __init__(self, x, y, monster_group, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('knight64.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=(x,y))
+        self.monster_group = monster_group
+        self.velocity = 5
+
+    def update(self):
+        self.move()
+        self.check_collisions()
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.rect.y += -self.velocity
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.velocity
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.velocity
+        if keys[pygame.K_LEFT]:
+            self.rect.x += -self.velocity
+
+    def check_collisions(self):
+        
+        if pygame.sprite.spritecollide(self, self.monster_group, True):
+            print(len(self.monster_group))
+
+        # for sprite in self.monster_group:
+        #     if pygame.sprite.collide_rect(self, sprite):
+        #         print('collided!')
+        #         sprite.kill()
+    
 # create a monster group and add 10 monsters
-monster_group = pygame.sprite.Group()
+my_monster_group = pygame.sprite.Group()
  
 for i in range(10):
-    x = random.randint(0, WINDOW_WIDTH-64) 
+    x = i * 70
     y = 20
-    Monster(x, y, monster_group)
+    Monster(x, y, my_monster_group)
+
+# create  a group class and a knight
+knight_group = pygame.sprite.Group()
+x = WINDOW_WIDTH // 2
+y = WINDOW_HEIGHT // 2
+knight = Knight(x, y, my_monster_group, knight_group)
+
 
 # main game loop
 running = True
@@ -48,19 +88,14 @@ while running:
     display_surface.fill('black')
 
     # draw the assets
-    monster_group.update()
-    # for sprite in monster_group.sprites():  下の方がよりシンプル
-    for sprite in monster_group:
-        if sprite.rect.y > WINDOW_HEIGHT-100:
-            #sprite.remove(monster_group)　　   これら３行は結果的に同じ働き
-            #monster_group.remove(sprite)
-            sprite.kill()
-            #sprite.remove()                    これはダメ。group名が必要
-            x = random.randint(0, WINDOW_WIDTH-64) 
-            y = 20
-            Monster(x, y, monster_group)
 
-    monster_group.draw(display_surface)
+    my_monster_group.update()
+    knight_group.update()
+
+ 
+    my_monster_group.draw(display_surface)
+    knight_group.draw(display_surface)
+
     pygame.display.update()
 
     clock.tick(FPS)
